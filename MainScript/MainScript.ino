f66cc1c6f902e5b9850f69c7342805d571fa3cbc
DexterBinary <declan.saul@gmail.com>
2024-05-09 08:59:45 +0200

    Added a template MainScript

---
+++ MainScript/MainScript.ino
@@ -0,0 +1,23 @@
+/*
+  Code by Declan Saul, Joshua Jackson-Smith and Connor Crighton of Parklands College Secondary for the 2024 NASA High Altitude Balloon Launch.
+*/
+
+/*
+  Checklist:
+  - Add Libraries
+  - Add SD Card code
+    *Two SD Cards
+    *Startup file functionality
+  - Add simple Layout
+  - Add Messages function  
+*/
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
